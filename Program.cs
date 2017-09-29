8bb5597d8adf3dcc7c078d6f7b040b14129d1863
morganwm <morgan.wm5@gmail.com>
2017-09-28 21:53:31 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reflectionCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
