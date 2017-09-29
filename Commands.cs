130c093607bbcf6809fcc9a9c6fc691b2ff4bbd3
morganwm <morgan.wm5@gmail.com>
2017-09-29 00:45:55 -0400

    huge amount of changes and started really digging into reflection

--- Commands.cs
+++ Commands.cs
@@ -3,35 +3,24 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 
-namespace reflectionCli
-{
-    public class ExitCommand : ICommand
-    {
-        public bool Execute()
-        {
-            return true;
-        }
-    }
+namespace reflectionCli {
+    public class commandset {
 
-    public class ParrotCommand : ICommand
-    {
-        
-        public ParrotCommand(List<string> input)
-        {
-            input.ForEach(x => Console.WriteLine(x));
-        }
-        
-        public bool Execute()
-        {
-            return false;
-        }
-    }
 
-    public class nullCommand : ICommand    
-    {
-        public bool Execute()
-        {
-            return false;
+
+        public class ParrotCommand : ICommand    {
+            
+            public ParrotCommand(List<string> input)
+            {
+                input.ForEach(x => Console.WriteLine(x));
+            }
+            
+            public bool ExitVal()
+            {
+                return false;
+            }
         }
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,50 +2,96 @@
 using System.Linq;
 using System.IO;
 using System.Collections.Generic;
+using System.Reflection;
 
-namespace reflectionCli
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace reflectionCli {
+    class Program    {
+        public static void Main(string[] args)  {
             var exit = false;
-            while(exit == false)
-            {
+            while(exit == false)    {
                 Console.WriteLine();
                 Console.WriteLine("Enter command (help to display help): "); 
                 var command = Parser.Parse(Console.ReadLine());
-                exit = command.Execute();
+                exit = command.ExitVal();
             }
         }
     }
 
-    public interface ICommand
-    {
-        bool Execute();
+    public interface ICommand    {
+        bool ExitVal();
     }
 
-    public static class Parser
-    {
+    public class nullCommand : ICommand    {
+        public bool ExitVal()        {
+            return false;
+        }
+    }
+
+    public class errorCommand : ICommand    {
+        
+        public errorCommand(string error)        {
+            Console.WriteLine(error);
+        }
+
+        public errorCommand() : this("Generic Error")   {
+
+        }
+        
+        public bool ExitVal()        {
+            return false;
+        }
+    }
+
+    public class ExitCommand : ICommand    {
+        public bool ExitVal()   {
+            return true;
+        }
+    }
+
+    public static class Parser    {
         public static ICommand Parse(string commandString) { 
+            
             // Parse your string and create Command object
             var commandParts = commandString.Split(' ').ToList();
             var commandName = commandParts[0];
-            var args = commandParts.Skip(1).ToList(); // the arguments is after the command
-            switch(commandName)
-            {
-                // Create command based on CommandName (and maybe arguments)
-                case "exit": 
-                    return new ExitCommand();
+            var args = commandParts.Skip(1).ToList(); // the arguments are after the command
 
-                case "parrot": 
-                    return new ParrotCommand(args);
-
-                default:
-                    Console.WriteLine("defaultcase");
-                    return new nullCommand();
-
+            Assembly assembly = typeof(commandset).GetTypeInfo().Assembly;
+            Type type = assembly.GetType("reflectionCli.commandset+" + commandName);
+            if (type == null)   {
+                return new errorCommand("Command Set Object Equals null");
             }
+
+            type.GetConstructors().ToList().ForEach(x => {
+                Console.WriteLine(x.Name);
+                x.GetParameters().ToList().ForEach(y => Console.WriteLine(" " + y.Name + "-" + y.ParameterType));
+            });
+
+
+            //type.GetMethods().ToList().ForEach(x => Console.WriteLine(x.Name));
+
+            // ConstructorInfo constructorInfo = type.GetConstructor(Type.EmptyTypes);
+            // if (constructorInfo == null)   {
+            //     Type[] _types = new Type[1];
+            //     _types[0] = typeof(List<string>);
+            //     constructorInfo = type.GetConstructor(_types);
+            //     if (constructorInfo == null)   {
+            //         return new errorCommand("constructor info is equal to null");
+            //     }
+            // }
+
+            // object result = null;
+            // ParameterInfo[] parameters = constructorInfo.GetParameters();
+            // object classInstance = Activator.CreateInstance(type, );
+
+            // if (parameters.Length == 0) {
+            //     result = constructorInfo.Invoke(classInstance, null);
+            // }
+            // else {
+            //     result = constructorInfo.Invoke(classInstance, new object[] { args } );
+            // }
+
+            return new errorCommand();
         }  
     }
 }
