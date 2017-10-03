cdc82d4721549355c1b62725a855904199121fa3
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-10-03 14:49:39 -0400

    andrews changes

--- Program.cs
+++ Program.cs
@@ -13,14 +13,13 @@ namespace reflectionCli {
         public static List<Assembly> activeasm;
 
         public static void Main(string[] args)  {
-            var exit = false;
             activeasm = new List<Assembly>();
             activeasm.Add(Assembly.GetEntryAssembly());
-            while(exit == false)    {
+            while(true)    {
                 Console.WriteLine();
                 Console.WriteLine("Enter command (help to display help): ");
                 var command = Parser.Parse(Console.ReadLine());
-                exit = command.ExitVal();
+                if (command.ExitVal()) { break; }
             }
         }
     }
