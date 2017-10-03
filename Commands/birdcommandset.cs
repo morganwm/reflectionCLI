555f8d929b132945a0514bd2508c1b5c2e240d01
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-10-03 15:53:29 -0400

    added an extra command to show the purpose of having an external cs file (basically it's for showing that you can call functions in classes)

--- Commands/birdcommandset.cs
+++ Commands/birdcommandset.cs
@@ -4,12 +4,23 @@ using System.IO;
 using System.Collections.Generic;
 
 namespace reflectionCli.extended {
-    public class commandset {
+    public class birdcommandset {
 
         public class Parrot : ICommand    {
 
             public Parrot(List<string> input) {
                 input.ForEach(x => Console.WriteLine(x));
+            }
+
+            public bool ExitVal() {
+                return false;
+            }
+        }
+
+        public class Parakeet : ICommand    {
+
+            public Parakeet(List<string> input) {
+                input.ForEach(x => Console.WriteLine("  +" + x));
             }
 
             public bool ExitVal() {
