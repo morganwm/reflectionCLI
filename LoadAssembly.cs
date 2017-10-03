0e7daf5c867f96cacf64e22846321ddfc94da736
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-10-03 14:39:08 -0400

    fixed system.exception to just exception

--- LoadAssembly.cs
+++ LoadAssembly.cs
@@ -16,11 +16,11 @@ namespace reflectionCli {
                 var tempicommand = TempAsm.GetTypes().ToList().Where(x => (x.Name == "ICommand")).ToList();
 
                 if (tempicommand.Count == 0) {
-                    throw new System.Exception("Unable to find ICommand");
+                    throw new Exception("Unable to find ICommand");
                 }
 
                 if (tempicommand.Count > 1) {
-                    throw new System.Exception("Multiple ICommands Found");
+                    throw new Exception("Multiple ICommands Found");
                 }
 
                 Type type = tempicommand[0];
@@ -31,14 +31,14 @@ namespace reflectionCli {
                                     .ToList();
 
                 if (extvalinfo.Count == 0) {
-                    throw new System.Exception("ICommands did not return the correct exit value");
+                    throw new Exception("ICommands did not return the correct exit value");
                 }
 
                 Program.activeasm.Add(TempAsm);
 
 
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
