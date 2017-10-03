0d723426d595745f07fe9eeff5bd7d492ea2db2f
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-10-03 15:48:52 -0400

    trimming out unneeded "using"

--- Commands/Standard/error.cs
+++ Commands/Standard/error.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Linq;
 using System.IO;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Runtime;
-using System.Runtime.Loader;
+
 
 namespace reflectionCli {
 
--- Commands/Standard/help.cs
+++ Commands/Standard/help.cs
@@ -3,8 +3,7 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime;
-using System.Runtime.Loader;
+
 
 namespace reflectionCli {
 
