e700b054c76df9e54257a4779904859b7f0aea72
morganwm <morgan.wm5@gmail.com>
2018-03-02 19:51:57 -0600

    exit tests

---
+++ src/ReflectionCli.Test/Standard/ExitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReflectionCli.Lib.Enums;
+using ReflectionCli.Test.HelperServices;
+using Xunit;
+using static ReflectionCli.extended.ScriptingCommandSet;
+
+namespace ReflectionCli.Test.Services
+{
+    public class ExitTests
+    {
+        private readonly FakeLoggingService _loggingService;
+        private readonly Exit _exit;
+
+        public ExitTests()
+        {
+            _loggingService = new FakeLoggingService();
+            _exit = new Exit(_loggingService);
+        }
+
+        [Fact]
+        public void Exit_ExitFunction_ShouldSetProgramExitToTrue()
+        {
+            _exit.Run();
+            
+            Assert.True(Program.ShutDown);
+        }
+    }
+}
